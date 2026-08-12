7cb59ac1edd0257f0f50304b6caeaf49f2c601b2
João Santos <81628111+joaovitrdos@users.noreply.github.com>
2026-08-12 19:53:50 -0300

    feat: adicioando mapper das entidades

--- src/main/java/com/layoof/layoof/mapper/ReactMapper.java
+++ src/main/java/com/layoof/layoof/mapper/ReactMapper.java
@@ -1,7 +1,7 @@
 package com.layoof.layoof.mapper;
 
-import com.layoof.layoof.dto.response.CommentReponseDto;
-import com.layoof.layoof.entity.Comment;
+import com.layoof.layoof.dto.response.ReactResponseDto;
+import com.layoof.layoof.entity.React;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
@@ -9,13 +9,13 @@ import org.mapstruct.ReportingPolicy;
 import java.util.List;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
-public interface CommentMapper {
+public interface ReactMapper {
 
-    @Mapping(target = "layoofId", source = "layoof.layoofId")
+    @Mapping(target = "commentId", source = "comment.commentId")
     @Mapping(target = "authorId", source = "author.userId")
     @Mapping(target = "authorName", source = "author.name")
     @Mapping(target = "authorPicture", source = "author.picture")
-    CommentReponseDto toResponse(Comment comment);
+    ReactResponseDto toResponse(React react);
 
-    List<CommentReponseDto> toResponseList(List<Comment> comments);
+    List<ReactResponseDto> toResponseList(List<React> reacts);
 }
